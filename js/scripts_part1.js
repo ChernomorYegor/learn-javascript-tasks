33b3745bf842819bc43549860d6d60c1f9671d71
ChernomorYegor <chernomor.yegor@gmail.com>
2021-04-14 01:39:13 +0300

    feat: add completed tasks 2.4.1, 2.4.2, 2.4.3 from part 1

---
+++ js/scripts_part1.js
@@ -0,0 +1,22 @@
+"use strict";
+// Part 1. The JavaScript language
+
+
+// 2.4 Variables
+
+// 2.4.1
+// let admin;
+// let name;
+//
+// name = "John";
+// admin = name;
+//
+// alert(admin); // "John"
+
+// 2.4.2
+// let ourPlanetName = "Earth";
+// let currentUserName = "John";
+
+// 2.4.3
+// const BIRTHDAY = '18.04.1982'; // Yes
+// const age = someCode(BIRTHDAY); // No
