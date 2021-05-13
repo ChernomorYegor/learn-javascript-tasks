e1eeb974f478224f58978201738a6081d4edfdf2
ChernomorYegor <chernomor.yegor@gmail.com>
2021-05-14 01:02:30 +0300

    feat: add completed tasks 4.5.1 - 4.5.3 from part 1

--- js/scripts-part1.js
+++ js/scripts-part1.js
@@ -534,3 +534,57 @@
 // };
 //
 // ladder.up().up().down().showStep(); // 1
+
+
+// 4.5 Constructor, operator "new"
+
+// 4.5.1
+// let obj = {};
+//
+// function A() {
+//   return obj;
+// }
+//
+// function B() {
+//   return obj;
+// }
+//
+// alert( new A() === new B() ); // true
+
+// 4.5.2
+// function Calculator() {
+//   this.read = function() {
+//     this.a = +prompt("a?", "0");
+//     this.b = +prompt("b?", "0");
+//   };
+//
+//   this.sum = function() {
+//     return this.a + this.b;
+//   };
+//
+//   this.mul = function() {
+//     return this.a * this.b;
+//   };
+// };
+//
+// let calculator = new Calculator();
+// calculator.read();
+//
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+
+// 4.5.3
+// function Accumulator(startingValue) {
+//   this.value = startingValue;
+//
+//   this.read = function() {
+//     this.value += +prompt("How much to add?", "0");
+//   }
+// }
+//
+// let accumulator = new Accumulator(1); // initial value 1
+//
+// accumulator.read(); // adds the user-entered value
+// accumulator.read(); // adds the user-entered value
+//
+// alert(accumulator.value); // shows the sum of these values
