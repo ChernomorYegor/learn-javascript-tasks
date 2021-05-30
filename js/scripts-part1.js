3186d5f8f3deb0dd67e6ece877678c7c967f6a20
ChernomorYegor <chernomor.yegor@gmail.com>
2021-05-30 19:52:31 +0300

    feat: add completed tasks 5.2.1 - 5.2.6 from part 1

--- js/scripts-part1.js
+++ js/scripts-part1.js
@@ -615,3 +615,67 @@
 // str.test = 5;
 //
 // alert(str.test); // It will show us an error (strict mode) and undefined (no strict mode)
+
+
+
+// 5.2 Numbers
+
+// 5.2.1
+// let num1 = +prompt("The first number?", "");
+// let num2 = +prompt("The second number?", "");
+//
+// alert(num1 + num2);
+
+// 5.2.2
+// alert( Math.round(6.35 * 10) / 10 ); // 6.4
+
+// 5.2.3
+// My solution:
+// function readNumber() {
+//   let number = prompt("Enter a number please?", "0");
+//
+//   if (!isFinite(+number)) {
+//     return readNumber();
+//   }
+//
+//   return number ? +number : null;
+// }
+//
+// alert(`Read: ${readNumber()}`);
+//
+// Original solution:
+// function readNumber() {
+//   let num;
+//
+//   do {
+//     num = prompt("Enter a number please?", 0);
+//   } while ( !isFinite(num) );
+//
+//   if (num === null || num === '') return null;
+//
+//   return +num;
+// }
+//
+// alert(`Read: ${readNumber()}`);
+
+// 5.2.4
+// let i = 0;
+// while (i != 10) { // Wrong comparison!!!
+//   i += 0.2;
+// }
+// The reason:
+// alert( 0.2.toFixed(20) );
+
+// 5.2.5
+// function random(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+//
+// alert( random(1, 5) );
+
+// 5.2.6
+// function randomInteger(min, max) {
+//   return Math.floor(Math.random() * (max + 1 - min) + min);
+// }
+//
+// alert( randomInteger(1, 5) );
