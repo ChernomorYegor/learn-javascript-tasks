0aa88bf79c77e0eb88d5e7ffa9a92d80a90fefa7
ChernomorYegor <chernomor.yegor@gmail.com>
2021-06-25 21:07:02 +0300

    feat: add completed tasks 5.9.1 and 5.9.2 from part 1

--- js/part1-5.js
+++ js/part1-5.js
@@ -553,3 +553,36 @@
 // readMessages.set(messages[0], Date.now());
 //
 // messages.shift();
+
+
+
+// 5.9 Object.keys, values, entries
+
+// 5.9.1
+// function sumSalaries(salaries) {
+//   let sum = 0;
+//
+//   for (let salary of Object.values(salaries)) {
+//     sum += salary;
+//   }
+//
+//   return sum;
+// }
+//
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+//
+// alert( sumSalaries(salaries) ); // 650
+//
+// Other solution:
+// function sumSalaries(salaries) {
+//   return Object.values(salaries).reduce((a, b) => a + b, 0)
+// }
+
+// 5.9.2
+// function count(obj) {
+//   return Object.keys(obj).length; // Also Object.values(obj).length or Object.entries(obj).length;
+// }
