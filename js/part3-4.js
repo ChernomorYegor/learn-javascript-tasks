039e6fec98d6482f7e97d5c1cff168cfb78054dd
ChernomorYegor <chernomor.yegor@gmail.com>
2022-01-07 22:05:02 +0300

    feat: add completed task 4.2.1 from part 3

---
+++ js/part3-4.js
@@ -0,0 +1,17 @@
+"use strict";
+// Part 3. Additional articles
+
+
+// 4.2 LocalStorage, sessionStorage
+
+// 4.2.1
+// area.value = localStorage.getItem('textarea');
+//
+// area.oninput = function() {
+//   localStorage.setItem('textarea', area.value);
+// };
+//
+// clear.onclick = function() {
+//   area.value='';
+//   localStorage.removeItem('textarea');
+// };
